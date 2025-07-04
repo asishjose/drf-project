4668e06d8851e5c4c39c7a540b6d54b00415cda1
Asish Jose <asishjosekakkattil@gmail.com>
2025-07-04 18:40:39 +0530

    added class based views

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # API endpoints here
-    path('students/', views.studentsView)
+    ## API endpoints here
+    # FBV examples in urls
+    path('students/', views.studentsView),
+    path('students/<int:pk>/', views.studentDetailView),
+
+
+    # CBV examples in urls
+    path('employees/', views.Employees.as_view()),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view()),
+
 ]
